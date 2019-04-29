e3333a498cff1ed6a77c9880e5ad46baf1ae382d
th-ang <namthang.law@gmail.com>
2019-04-29 22:17:49 +0700

    update dim eff

--- another.js
+++ another.js
@@ -1,5 +1,33 @@
-const tSharpElements = document.getElementsByName('link-t-sharp');
+const tSharpElements = document.getElementsByClassName('t-sharp-elements')[0];
+const tSharpLinks = document.getElementsByClassName('t-sharp-nav')[0];
 
-tSharpLink.forEach(()=> {
-    
-})
+function fadeIn(el){
+    el.classList.add('show');
+    el.classList.remove('dim');  
+  }
+  
+  function dim(el){
+    el.classList.add('dim');
+    el.classList.remove('show');
+ 
+  }
+  
+
+  tSharpLinks.addEventListener('mouseover', function(){
+    if (tSharpElements.className.indexOf('dim') !== -1) {
+      fadeIn(tSharpElements);
+     
+    }
+    else {
+      dim(tSharpElements);
+    }
+  });
+  tSharpLinks.addEventListener('mouseout', function(){
+    if (tSharpElements.className.indexOf('dim') !== -1) {
+      fadeIn(tSharpElements);
+     
+    }
+    else {
+      dim(tSharpElements);
+    }
+  });
